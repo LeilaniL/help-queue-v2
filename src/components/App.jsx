9874fb82f6b0191a31f0a571a3a7320f8d068f5a
Leilani Leach and Miranda Keith <leilani@>
2019-02-25 14:47:07 -0800

    refactor ticket creation to use redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,14 +16,13 @@ class App extends React.Component {
       masterTicketList: {},
       selectedTicket: null
     };
-    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
     this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
   }
 
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
       this.updateTicketElapsedWaitTime(),
-      5000
+      60000
     );
   }
 
@@ -39,26 +38,18 @@ class App extends React.Component {
     this.setState({ masterTicketList: newMasterTicketList });
   }
 
-  handleAddingNewTicketToList(newTicket) {
-    var newTicketId = v4();
-    var newMasterTicketList = Object.assign({}, this.state.masterTicketList, {
-      [newTicketId]: newTicket
-    });
-    newMasterTicketList[newTicket.id].formattedWaitTime = newMasterTicketList[newTicket.id].timeOpen.fromNow(true);
-    this.setState({ masterTicketList: newMasterTicketList })
-  }
-
   handleChangingSelectedTicket(ticketId) {
     this.setState({ selectedTicket: ticketId });
   }
 
   render() {
+    console.log(this.state.masterTicketList);
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path='/' render={() => <TicketList ticketList={this.state.masterTicketList} />} />
-          <Route path='/newticket' render={() => <NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route path='/newticket' render={() => <NewTicketControl />} />
           <Route path='/admin' render={(props) => <Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}
             onTicketSelection={this.handleChangingSelectedTicket}
             selectedTicket={this.state.selectedTicket} />} />
